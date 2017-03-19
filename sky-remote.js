861c28e3c7b5d9906c909aa82b87b6597d525ccd
Dal Hundal <dal@dalhundal.com>
2017-03-19 16:09:07 +0000

    Handle trying to connect to connect to incorrect address by throwing a timeout error

--- sky-remote.js
+++ sky-remote.js
@@ -2,6 +2,9 @@ var net = require('net');
 
 function SkyRemote(host, port) {
 
+	var that = this;
+	this.connectTimeout = 1000;
+
 	function sendCommand(code, cb) {
 		var commandBytes = [4,1,0,0,0,0, Math.floor(224 + (code/16)), code % 16];
 
@@ -12,6 +15,7 @@ function SkyRemote(host, port) {
 
 		var l = 12;
 		client.on('data', function(data) {
+			clearTimeout(connectTimeoutTimer)
 			// Clear timeout
 			if (data.length < 24) {
 				client.write(data.slice(0, l))
@@ -26,20 +30,34 @@ function SkyRemote(host, port) {
 				});
 			}
 		});
+
+		client.on('error', function(err) {
+			clearTimeout(connectTimeoutTimer)
+			cb(err)
+		})
+
+		var connectTimeoutTimer = setTimeout(function() {
+			client.end()
+			var err = new Error('connect timeout '+host+':'+port)
+			err.name = 'ECONNTIMEOUT'
+			err.address = host
+			err.port = port
+			cb(err)
+		}, that.connectTimeout)
 	}
 
 	this.press = function press(sequence, cb) {
 		if (typeof sequence !== 'object' || !sequence.hasOwnProperty('length')) {
 			return press(sequence.split(','), cb)
 		};
-		sendCommand(SkyRemote.commands[sequence.shift()],function() {
+		sendCommand(SkyRemote.commands[sequence.shift()],function(err) {
 			if (sequence.length) {
 				setTimeout(function() {
 					press(sequence, cb);
 				},500);
 			} else {
 				if (typeof cb === 'function') {
-					cb();
+					cb(err);
 				}
 			}
 		});
